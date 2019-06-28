c3106b6699402d71fc3af9109d9fe2073f4bf587
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2019-06-29 00:23:09 +0200

    updated dropdown

---
+++ VRestionnairesUnity/Assets/VRestionnaires/Scripts/UI/Skin/SkinDropdown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+namespace VRestionnaire {
+	[RequireComponent(typeof(TMP_Dropdown))]
+	public class SkinDropdown:MonoBehaviour, ISkinHandler {
+
+		public void ApplySkin(UISkinData skin)
+		{
+			TMP_Dropdown dropdown = GetComponent<TMP_Dropdown>();
+			dropdown.colors = skin.buttonColorBlock;
+		}
+	}
+
+}
