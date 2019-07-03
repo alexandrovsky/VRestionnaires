695d14a47b4f8036f0b1481d759e6f68546d840a
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2019-07-03 18:58:07 +0200

    relative scale

--- VRestionnairesUnity/Assets/VRestionnaires/Scripts/Utils/LookAt.cs
+++ VRestionnairesUnity/Assets/VRestionnaires/Scripts/Utils/LookAt.cs
@@ -17,9 +17,9 @@ public class LookAt : MonoBehaviour
 		ht.Add(iT.ScaleTo.time, 1);
 		ht.Add(iT.ScaleTo.looptype,"pingPong");
 		ht.Add(iT.ScaleTo.delay, 0);
-		ht.Add(iT.ScaleTo.x,1.25f);
-		ht.Add(iT.ScaleTo.y,1.25f);
-		ht.Add(iT.ScaleTo.z,1.25f);
+		ht.Add(iT.ScaleTo.x, arrow.transform.localScale.x * 1.25f);
+		ht.Add(iT.ScaleTo.y, arrow.transform.localScale.y * 1.25f);
+		ht.Add(iT.ScaleTo.z, arrow.transform.localScale.z * 1.25f);
 		iTween.ScaleTo(arrow, ht);
 	}
 
