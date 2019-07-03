fe2a7ae2fe0fd82d4c5a29aac4b8c97843e2db79
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2019-07-03 18:52:05 +0200

    added arrow indicator

---
+++ VRestionnairesUnity/Assets/VRestionnaires/Scripts/Utils/LookAt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+	public Transform target;
+	public GameObject arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+
+		Hashtable ht = new Hashtable();
+
+		//ht.Add(iT.ScaleBy.looptype,iTween.LoopType.pingPong);
+		ht.Add(iT.ScaleTo.time, 1);
+		ht.Add(iT.ScaleTo.looptype,"pingPong");
+		ht.Add(iT.ScaleTo.delay, 0);
+		ht.Add(iT.ScaleTo.x,1.25f);
+		ht.Add(iT.ScaleTo.y,1.25f);
+		ht.Add(iT.ScaleTo.z,1.25f);
+		iTween.ScaleTo(arrow, ht);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(target) {
+			transform.LookAt(target,Vector3.up);
+		}
+    }
+}
